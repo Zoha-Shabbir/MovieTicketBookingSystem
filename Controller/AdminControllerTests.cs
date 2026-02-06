800656b4c1eb8e6ae01c097ad11d00ad733cd7e7
Zoha-Shabbir <153354736+Zoha-Shabbir@users.noreply.github.com>
2026-02-06 18:58:05 +0500

    Add files via upload

---
+++ Controller/AdminControllerTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using MovieBooking.Controllers;
+using MovieBooking.Hubs;
+using MovieBooking.Models;
+using MovieBooking.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieBooking.Tests.Controller
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void Index_Returns_View()
+        {
+            var movieRepo = new Mock<IMovieRepository>();
+            var bookingRepo = new Mock<IBookingRepository>();
+            var hub = new Mock<IHubContext<MovieHub>>();
+
+            var controller = new AdminController(
+                movieRepo.Object,
+                bookingRepo.Object,
+                hub.Object
+            );
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task AddMovie_Redirects_To_Index()
+        {
+            var movieRepo = new Mock<IMovieRepository>();
+            var bookingRepo = new Mock<IBookingRepository>();
+
+            var mockClientProxy = new Mock<IClientProxy>();
+            mockClientProxy
+                .Setup(x => x.SendCoreAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<object[]>(),
+                    default))
+                .Returns(Task.CompletedTask);
+
+            var mockClients = new Mock<IHubClients>();
+            mockClients.Setup(c => c.All).Returns(mockClientProxy.Object);
+
+            var hub = new Mock<IHubContext<MovieHub>>();
+            hub.Setup(h => h.Clients).Returns(mockClients.Object);
+
+            var controller = new AdminController(
+                movieRepo.Object,
+                bookingRepo.Object,
+                hub.Object
+            );
+
+            controller.TempData = new Mock<ITempDataDictionary>().Object;
+
+            var result = await controller.AddMovie("Avatar", "Sci-Fi", 500, "url", 10);
+
+            var redirect = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirect.ActionName);
+
+            mockClientProxy.Verify(x => x.SendCoreAsync(It.IsAny<string>(), It.IsAny<object[]>(), default), Times.Once);
+        }
+    
+        [Fact]
+        public async Task AddMovie_InvalidData_Returns_ViewWithError()
+        {
+            var movieRepo = new Mock<IMovieRepository>();
+            var bookingRepo = new Mock<IBookingRepository>();
+
+            var mockClientProxy = new Mock<IClientProxy>();
+            mockClientProxy.Setup(x => x.SendCoreAsync(It.IsAny<string>(), It.IsAny<object[]>(), default))
+                           .Returns(Task.CompletedTask);
+
+            var mockClients = new Mock<IHubClients>();
+            mockClients.Setup(c => c.All).Returns(mockClientProxy.Object);
+
+            var hub = new Mock<IHubContext<MovieHub>>();
+            hub.Setup(h => h.Clients).Returns(mockClients.Object);
+
+            var controller = new AdminController(movieRepo.Object, bookingRepo.Object, hub.Object);
+            controller.TempData = new Mock<ITempDataDictionary>().Object;
+
+            var result = await controller.AddMovie("", "Genre", 100, "url", 5);
+
+            var redirect = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirect.ActionName);
+        }
+        [Fact]
+        public async Task AddMovie_RepositoryThrows_ExceptionHandled()
+        {
+            var movieRepo = new Mock<IMovieRepository>();
+            movieRepo.Setup(m => m.AddAsync(It.IsAny<Movie>()))
+                     .ThrowsAsync(new Exception("Database error"));
+
+            var bookingRepo = new Mock<IBookingRepository>();
+
+            var mockClientProxy = new Mock<IClientProxy>();
+            var mockClients = new Mock<IHubClients>();
+            mockClients.Setup(c => c.All).Returns(mockClientProxy.Object);
+
+            var hub = new Mock<IHubContext<MovieHub>>();
+            hub.Setup(h => h.Clients).Returns(mockClients.Object);
+
+            var controller = new AdminController(movieRepo.Object, bookingRepo.Object, hub.Object);
+            controller.TempData = new Mock<ITempDataDictionary>().Object;
+
+            await Assert.ThrowsAsync<Exception>(() => controller.AddMovie("Avatar", "Sci-Fi", 500, "url", 10));
+        }
+
+
+    }
+
+}
---
+++ Controller/BookingControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using MovieBooking.Controllers;
+using MovieBooking.Hubs;
+using MovieBooking.Models;
+using MovieBooking.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+namespace MovieBooking.Tests.Controller
+{
+    public class BookingControllerTests
+    {
+        private readonly Mock<IMovieRepository> _movieRepoMock;
+        private readonly Mock<IBookingRepository> _bookingRepoMock;
+        private readonly Mock<IHubContext<MovieHub>> _hubMock;
+        private readonly Mock<UserManager<ApplicationUser>> _userManagerMock;
+
+        public BookingControllerTests()
+        {
+            _movieRepoMock = new Mock<IMovieRepository>();
+            _bookingRepoMock = new Mock<IBookingRepository>();
+            _hubMock = new Mock<IHubContext<MovieHub>>();
+
+            var store = new Mock<IUserStore<ApplicationUser>>();
+            _userManagerMock = new Mock<UserManager<ApplicationUser>>(
+                store.Object, null, null, null, null, null, null, null, null);
+        }
+
+        [Fact]
+        public async Task Index_ReturnsViewWithMovies()
+        {
+            var controller = new BookingController(
+                _movieRepoMock.Object,
+                _bookingRepoMock.Object,
+                _hubMock.Object,
+                _userManagerMock.Object
+            );
+
+            var result = await controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task ToBook_InvalidMovie_ReturnsNotFound()
+        {
+            _movieRepoMock.Setup(x => x.GetByIdAsync(1)).ReturnsAsync((Movie)null);
+
+            var controller = new BookingController(
+                _movieRepoMock.Object,
+                _bookingRepoMock.Object,
+                _hubMock.Object,
+                _userManagerMock.Object
+            );
+
+            var result = await controller.ToBook(1);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
